295b4147537f8922ae8428e9e27670fc4ac10cc8
vaishnavi-blauplug <vaishnavi.kalkute@blauplug.com>
2026-04-02 09:33:08 +0530

    cors chanegs

--- app/config.py
+++ app/config.py
@@ -6,3 +6,5 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60"))
+# CORS
+CORS_ORIGINS: str =os.getenv("CORS_ORIGINS")
--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.auth import router as auth_router
 from app.projects import router as projects_router
-
+from config import CORS_ORIGINS
 app = FastAPI()
 
 # CORS (for React / Next.js)
+# CORS - configured via CORS_ORIGINS env variable
+cors_origins = [origin.strip() for origin in CORS_ORIGINS.split(",") if origin.strip()]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"], #restrict origins only to frontend 
+    allow_origins=cors_origins,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
